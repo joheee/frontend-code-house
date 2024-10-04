63cac8f77b9666a6476ff44822d873d88f422fc4
johe lappy <johevin.blesstowi@binus.ac.id>
2024-10-05 05:20:25 +0700

    testing image

--- src/app/landing/LandingDesktop.tsx
+++ src/app/landing/LandingDesktop.tsx
@@ -1,5 +1,9 @@
+import Image from "next/image";
+
 export default function LandingDesktop() {
   return (
-    <div>LandingDesktop</div>
+    <div>
+      <Image src='/landing_background.png' width={100} height={100} alt="bla bla"/>
+    </div>
   )
 }
