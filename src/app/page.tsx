d81cc99b8626987bf105906f0fc5bb2452174c0e
johe lappy <johevin.blesstowi@binus.ac.id>
2024-10-04 19:42:48 +0700

    component: src/navigation/NavigationDesktop.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
+import NavigationDesktop from "@/navigation/NavigationDesktop";
+
 export default function Home() {
   return (
     <div className="">
-      <div className="flex justify-between items-center py-5 px-10 text-md">
-        <div className="font-bold">Code House</div>
-        <div className="bg-white shadow-md flex p-2 gap-2 rounded-sm">
-          <div className="cursor-pointer hover:bg-accent px-4 py-[1] rounded-sm text-font_dark">Home</div>
-          <div className="cursor-pointer hover:bg-accent px-4 py-[1] rounded-sm text-font_dark">About</div>
-          <div className="cursor-pointer hover:bg-accent px-4 py-[1] rounded-sm text-font_dark">Works</div>
-          <div className="cursor-pointer hover:bg-accent px-4 py-[1] rounded-sm text-font_dark">Contact</div>
-        </div>
-        <div className="hover:bg-accent_hover hover:cursor-pointer bg-accent px-4 py-1 rounded-sm text-font_dark">Get in touch</div>
-      </div>
+      <NavigationDesktop />
     </div>
   );
 }
--- src/navigation/NavigationDesktop.tsx
+++ src/navigation/NavigationDesktop.tsx
@@ -1,14 +1,24 @@
 export default function NavigationDesktop() {
   return (
-    <div className="flex justify-center">
-      <div className="">Code House</div>
-      <div className="bg-white shadow-md">
-        <div className="">Home</div>
-        <div className="">About</div>
-        <div className="">Works</div>
-        <div className="">Contact</div>
+    <div className="flex justify-between items-center py-5 px-10 text-md">
+      <div className="font-bold">Code House</div>
+      <div className="bg-white shadow-md flex p-2 gap-2 rounded-sm">
+        <div className="cursor-pointer hover:bg-accent px-4 py-[1] rounded-sm text-font_dark">
+          Home
+        </div>
+        <div className="cursor-pointer hover:bg-accent px-4 py-[1] rounded-sm text-font_dark">
+          About
+        </div>
+        <div className="cursor-pointer hover:bg-accent px-4 py-[1] rounded-sm text-font_dark">
+          Works
+        </div>
+        <div className="cursor-pointer hover:bg-accent px-4 py-[1] rounded-sm text-font_dark">
+          Contact
+        </div>
       </div>
-      <div className="">Get in touch</div>
+      <div className="hover:bg-accent_hover hover:cursor-pointer bg-accent px-4 py-1 rounded-sm text-font_dark">
+        Get in touch
+      </div>
     </div>
   );
 }
