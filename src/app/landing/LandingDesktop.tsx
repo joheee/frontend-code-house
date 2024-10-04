015e7e13231d1406dd44d41ebdb0846d8729d2d2
johe lappy <johevin.blesstowi@binus.ac.id>
2024-10-05 06:00:29 +0700

    landing desktop

--- src/app/landing/LandingDesktop.tsx
+++ src/app/landing/LandingDesktop.tsx
@@ -4,12 +4,40 @@ export default function LandingDesktop() {
   return (
     <div className="">
       <Image
+        className="z-0"
         src="/landing_background.png"
-        alt="bla bla"
-        layout="fill" 
-        quality={100} 
-        priority 
+        alt="landing page background for Code House"
+        layout="fill"
+        quality={100}
+        priority
       />
+      <div className="z-10 flex flex-col items-center justify-center h-screen">
+        <div className="flex gap-5 items-center">
+          <div className="font-bold text-[65px]">WE ARE</div>
+          <Image
+            className="w-fit h-fit z-0"
+            src="/landing_first_content.png"
+            alt="landing page background for Code House"
+            width={100}
+            height={100}
+            quality={100}
+            priority
+          />
+          <div className="font-bold text-[65px]">SOFTWARE</div>
+        </div>
+        <div className="flex gap-5 items-center">
+          <div className="font-bold text-[65px]">HOUSE</div>
+          <Image
+            className="w-fit h-fit z-0"
+            src="/landing_second_content.png"
+            alt="landing page background for Code House"
+            width={100}
+            height={100}
+            quality={100}
+            priority
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/navigation/NavigationComponent.tsx
+++ src/app/navigation/NavigationComponent.tsx
@@ -3,7 +3,7 @@ import NavigationMobile from "./NavigationMobile";
 
 export default function NavigationComponent() {
   return (
-    <div>
+    <div className="fixed w-screen z-10">
       <NavigationDesktop className="hidden tablet:flex" />
       <NavigationMobile className="block tablet:hidden" />
     </div>
