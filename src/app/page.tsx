3942df6c31441df2bc09f78a47ec6201a2072972
johe lappy <johevin.blesstowi@binus.ac.id>
2024-10-07 04:44:46 +0700

    service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import FooterComponent from "./footer/FooterComponent";
 import KeyFactComponent from "./keyFact/KeyFactComponent";
 import LandingComponent from "./landing/LandingComponent";
 import NavigationComponent from "./navigation/NavigationComponent";
-import ServiceComponent from "./services/ServiceComponent";
+import ServiceComponent from "./service/ServiceComponent";
 import WorkComponent from "./work/WorkComponent";
 
 export default function Home() {
