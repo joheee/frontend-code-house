ba2711e92530c9011478d7cf6e1c4de1700ccd23
johe lappy <johevin.blesstowi@binus.ac.id>
2024-10-07 04:40:26 +0700

    service page add text dark and light

--- src/app/services/ServiceCard.tsx
+++ src/app/services/ServiceCard.tsx
@@ -2,7 +2,7 @@ import { ServiceCardInterface } from "./interface";
 
 export default function ServiceCard(prop: ServiceCardInterface) {
   return (
-    <div className="bg-white rounded-sm shadow-md p-5">
+    <div className="bg-white rounded-sm shadow-md p-5 text-font_dark">
       <div className="text-clamp-service-card-title font-semibold">
         {prop.title}
       </div>
