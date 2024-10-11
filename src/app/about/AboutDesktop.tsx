49b26872e082688b0e9ed5bb58975b785945193b
johe lappy <johevin.blesstowi@binus.ac.id>
2024-10-11 16:10:37 +0700

    add motion frammer for about page

--- src/app/about/AboutDesktop.tsx
+++ src/app/about/AboutDesktop.tsx
@@ -1,14 +1,43 @@
 import AboutContent from "./AboutContent";
 import { AboutInterface } from "./interface";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 export default function AboutDesktop(about: AboutInterface) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false, amount: .3 });
+
+  const fadeInUp = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.8,
+        ease: "easeOut",
+      },
+    },
+  };
+
   return (
-    <div className={`${about.className} grid grid-cols-2 items-center`}>
+    <motion.div
+      ref={ref} 
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      variants={fadeInUp}
+      className={`${about.className} grid grid-cols-2 items-center`}
+    >
       <div className="">
-        <div className="font-extrabold text-[50px]">ABOUT THE</div>
-        <div className="font-extrabold text-[50px]">CODE HOUSE</div>
+        <motion.div className="font-extrabold text-[50px]" variants={fadeInUp}>
+          ABOUT THE
+        </motion.div>
+        <motion.div className="font-extrabold text-[50px]" variants={fadeInUp}>
+          CODE HOUSE
+        </motion.div>
       </div>
-      <AboutContent />
-    </div>
+      <motion.div variants={fadeInUp}>
+        <AboutContent />
+      </motion.div>
+    </motion.div>
   );
 }
--- src/app/about/AboutMobile.tsx
+++ src/app/about/AboutMobile.tsx
@@ -1,14 +1,36 @@
+import { useRef } from "react";
 import AboutContent from "./AboutContent";
 import { AboutInterface } from "./interface";
+import { motion, useInView } from "framer-motion";
 
 export default function AboutMobile(about: AboutInterface) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false, amount: 0.3 });
+
+  const fadeInUp = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.8,
+        ease: "easeOut",
+      },
+    },
+  };
   return (
-    <div className={`${about.className} grid items-center`}>
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      variants={fadeInUp}
+      className={`${about.className} grid items-center`}
+    >
       <div className="">
         <div className="font-extrabold text-clamp-about">ABOUT THE</div>
         <div className="font-extrabold text-clamp-about">CODE HOUSE</div>
       </div>
       <AboutContent />
-    </div>
+    </motion.div>
   );
 }
